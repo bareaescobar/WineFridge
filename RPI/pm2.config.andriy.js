f23ea93a46bf63727fdc7064155d24554a79a017
bareaescobar <bareaescobar@gmail.com>
2025-07-15 15:30:16 +0100

    Keep local pm2.config.js and add pm2.config.andriy.js from upstream

--- RPI/pm2.config.andriy.js
+++ RPI/pm2.config.andriy.js
@@ -3,28 +3,24 @@ module.exports = {
     {
       name: 'mqtt-handler',
       script: 'mqtt_handler.py',
-      interpreter: '/usr/bin/python3',
+      interpreter: 'python3',
       cwd: './backend'
     },
     {
       name: 'barcode-scanner',
       script: 'barcode_scanner.py',
-      interpreter: '/usr/bin/python3',
+      interpreter: 'python3',
       cwd: './backend'
     },
     {
       name: 'web-server',
-      script: 'server.cjs',
-      cwd: './frontend/backend',
-      interpreter: 'node',
-      env: {
-        NODE_ENV: 'production'
-      }
+	    script: 'server.cjs',
+      cwd: './frontend/backend'
     },
     {
       name: 'kiosk',
       script: 'chromium-browser',
-      args: '--kiosk --noerrdialogs --disable-infobars --disable-session-crashed-bubble --disable-translate --incognito --disable-cache --app=http://localhost:3000 --ozone-platform=wayland --disable-features=WaylandWindowDecorations --log-level=3',
+      args: '--kiosk --noerrdialogs --disable-infobars --incognito --app=http://localhost:3000 --ozone-platform=wayland --disable-features=WaylandWindowDecorations --log-level=3',
       exec_mode: 'fork',
       interpreter: 'none',
       env: {
